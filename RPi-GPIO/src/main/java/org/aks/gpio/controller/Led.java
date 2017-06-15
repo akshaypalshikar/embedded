c43603053740effa5408d88b47af543e9964df8d
aksma <aksma@DESKTOP-0FV5PJQ>
2017-06-14 19:41:48 -0700

    spring boot project for led

---
+++ RPi-GPIO/src/main/java/org/aks/gpio/controller/Led.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.aks.gpio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+/**
+ * @author Akshay
+ *
+ */
+@RestController
+public class Led {
+
+	private static GpioPinDigitalOutput gpioOutputPin;
+
+	@RequestMapping("/")
+	public String greeting() {
+		return "Hello world!";
+	}
+
+	@RequestMapping("/light")
+	public String light() {
+		GpioController gpioController = GpioFactory.getInstance();
+		if (gpioOutputPin==null) {
+			gpioOutputPin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+		}
+		gpioOutputPin.toggle();
+		return null;
+	}
+}
